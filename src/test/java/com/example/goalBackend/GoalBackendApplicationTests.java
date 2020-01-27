97490104699c2a63713dd15379ffa60a882ed83d
Ken Sevilla <ken.michael.sevilla@oocl.com>
2020-01-27 08:47:54 +0800

    initialize project

---
+++ src/test/java/com/example/goalBackend/GoalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goalBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
