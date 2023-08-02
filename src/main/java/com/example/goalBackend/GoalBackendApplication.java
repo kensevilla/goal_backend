30f354f3de03af0443a00f8addaff62a8ae604e3
Ken Sevilla <kenmichaelsev@gmail.com>
2023-08-02 20:54:36 +0800

    fix reading firebase config

--- src/main/java/com/example/goalBackend/GoalBackendApplication.java
+++ src/main/java/com/example/goalBackend/GoalBackendApplication.java
@@ -6,21 +6,20 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Objects;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class GoalBackendApplication {
 
 	public static void main(String[] args) throws IOException {
 		ClassLoader classLoader = GoalBackendApplication.class.getClassLoader();
-		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
-		FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+		InputStream inputStream = classLoader.getResourceAsStream("serviceAccountKey.json");
+
+
 
 		FirebaseOptions options = new FirebaseOptions.Builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.setCredentials(GoogleCredentials.fromStream(inputStream))
 				.setDatabaseUrl("https://goal-app-da9d5-default-rtdb.asia-southeast1.firebasedatabase.app")
 				.build();
 
@@ -28,5 +27,4 @@ public class GoalBackendApplication {
 
 		SpringApplication.run(GoalBackendApplication.class, args);
 	}
-
 }
